e74b0ef0e942dcb23ec206a4757dcdc8951a4f3f
seerosecode <keshalakemusic@gmail.com>
2019-04-28 08:40:21 -0400

    halfway there YAY FOR PROGRESS

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,38 @@
 #START OVER
+require 'pry'
+class Artist
+
+attr_accessor :name
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    genres = []
+    Song.all.each do |song|
+      if song.artist == self
+        genres << song.genre
+      end
+    end
+    genres
+  end
+
+
+end
